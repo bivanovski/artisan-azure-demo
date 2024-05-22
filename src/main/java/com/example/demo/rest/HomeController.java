b0f1078beafde8332044d4cb45f61d04fd4bb8de
BoIv1412 <bojan.ivanovski@studentambassadors.com>
2024-05-22 23:30:17 +0200

    added home controller for swagger

---
+++ src/main/java/com/example/demo/rest/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.demo.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/swagger-ui.html";
+    }
+}
